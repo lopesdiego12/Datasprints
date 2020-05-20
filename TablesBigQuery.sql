a343a171f94b5c0a4cd7e73425e702c680faa1b1
lopesdiego12 <lopesdiego12@gmail.com>
2020-05-20 07:34:34 -0300

    Second commit

---
+++ TablesBigQuery.sql
@@ -0,0 +1,35 @@
+Trips
+
+vendor_id			STRING	NULLABLE	
+pickup_datetime		TIMESTAMP	NULLABLE	
+dropoff_datetime	TIMESTAMP	NULLABLE	
+passenger_count		INTEGER	NULLABLE	
+trip_distance		FLOAT	NULLABLE	
+pickup_longitude	FLOAT	NULLABLE	
+pickup_latitude		FLOAT	NULLABLE	
+rate_code			STRING	NULLABLE	
+store_and_fwd_flag	STRING	NULLABLE	
+dropoff_longitude	FLOAT	NULLABLE	
+dropoff_latitude	FLOAT	NULLABLE	
+payment_type		STRING	NULLABLE	
+fare_amount			FLOAT	NULLABLE	
+surcharge			FLOAT	NULLABLE	
+tip_amount			FLOAT	NULLABLE	
+tolls_amount		FLOAT	NULLABLE	
+total_amount		FLOAT	NULLABLE
+
+Payment	
+
+payment_type	STRING	NULLABLE	
+payment_lookup	STRING	NULLABLE	
+
+Vendor
+vendor_id	STRING	NULLABLE	
+name		STRING	NULLABLE	
+address		STRING	NULLABLE	
+city		STRING	NULLABLE	
+state		STRING	NULLABLE	
+zip			STRING	NULLABLE	
+country		STRING	NULLABLE	
+contact		STRING	NULLABLE	
+current		STRING	NULLABLE	
