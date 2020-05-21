7d088f44d6319e9a7346822f95123f3ca63e6550
Diego Lopes <lopesdiego12@gmail.com>
2020-05-21 09:01:06 -0300

    Update TablesBigQuery.sql

--- TablesBigQuery.sql
+++ TablesBigQuery.sql
@@ -1,22 +1,25 @@
+Tabelas no Big Query
+
 Trips
 
-vendor_id			STRING	NULLABLE	
+vendor_id			STRING		NULLABLE	
 pickup_datetime		TIMESTAMP	NULLABLE	
 dropoff_datetime	TIMESTAMP	NULLABLE	
-passenger_count		INTEGER	NULLABLE	
-trip_distance		FLOAT	NULLABLE	
-pickup_longitude	FLOAT	NULLABLE	
-pickup_latitude		FLOAT	NULLABLE	
-rate_code			STRING	NULLABLE	
-store_and_fwd_flag	STRING	NULLABLE	
-dropoff_longitude	FLOAT	NULLABLE	
-dropoff_latitude	FLOAT	NULLABLE	
-payment_type		STRING	NULLABLE	
-fare_amount			FLOAT	NULLABLE	
-surcharge			FLOAT	NULLABLE	
-tip_amount			FLOAT	NULLABLE	
-tolls_amount		FLOAT	NULLABLE	
-total_amount		FLOAT	NULLABLE
+passenger_count		INTEGER		NULLABLE	
+trip_distance		FLOAT		NULLABLE	
+pickup_longitude	FLOAT		NULLABLE	
+pickup_latitude		FLOAT		NULLABLE 	
+rate_code			STRING		NULLABLE	
+store_and_fwd_flag	STRING		NULLABLE	
+dropoff_longitude	FLOAT		NULLABLE	
+dropoff_latitude	FLOAT		NULLABLE	
+payment_type		STRING		NULLABLE	
+fare_amount			FLOAT		NULLABLE	
+surcharge			FLOAT		NULLABLE	
+tip_amount			FLOAT		NULLABLE	
+tolls_amount		FLOAT		NULLABLE	
+total_amount		FLOAT		NULLABLE
+
 
 Payment	
 
@@ -24,6 +27,7 @@ payment_type	STRING	NULLABLE
 payment_lookup	STRING	NULLABLE	
 
 Vendor
+
 vendor_id	STRING	NULLABLE	
 name		STRING	NULLABLE	
 address		STRING	NULLABLE	
